a6532b02dbeb6792f3ed6d831feb1b948b51864f
saiket-das <saiketdas02@gmail.com>
2024-06-13 12:29:14 +0600

    Create user model and interface

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,37 @@
+import { Schema } from "mongoose";
+import { UserProps } from "./user.interface";
+import { UserRole } from "./user.constant";
+
+const mongoose = require("mongoose");
+
+const userSchema = new Schema<UserProps>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: UserRole,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
