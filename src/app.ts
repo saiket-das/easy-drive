19994a6390eb1f890fa505562999acbe626a98e3
saiket-das <saiketdas02@gmail.com>
2024-06-13 13:57:21 +0600

    Implement global error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -14,5 +15,8 @@ app.use("/api", router);
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running!");
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
