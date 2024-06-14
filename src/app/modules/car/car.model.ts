974f092a5ee17319324f8e9fcaf878116f76d609
saiket-das <saiketdas02@gmail.com>
2024-06-15 05:46:19 +0600

    Add route to delete a Car (only accessible to Admin)

--- src/app/modules/car/car.model.ts
+++ src/app/modules/car/car.model.ts
@@ -1,5 +1,7 @@
 import { model, Schema } from "mongoose";
 import { CarProps } from "./car.interface";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
 
 const carSchema = new Schema<CarProps>(
   {
@@ -41,4 +43,25 @@ const carSchema = new Schema<CarProps>(
   }
 );
 
+// Pre-find hook to exclude deleted cars before fatching
+carSchema.pre("find", function (next) {
+  this.where({ isDeleted: { $ne: true } });
+  next();
+});
+
+carSchema.pre("findOne", function (next) {
+  this.where({ isDeleted: { $ne: true } });
+  next();
+});
+
+// Pre-hook to check if car exists or not
+carSchema.pre("findOneAndUpdate", async function (next) {
+  const id = this.getQuery()._id;
+
+  const car = await CarModel.findById(id);
+  if (!car) {
+    throw new AppError(httpStatus.NOT_FOUND, "Car not found");
+  }
+  next();
+});
 export const CarModel = model<CarProps>("Car", carSchema);
