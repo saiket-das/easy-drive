36ee16408745a09a7e6a3d0ec8d91779dfef5a35
saiket-das <saiketdas02@gmail.com>
2024-06-16 02:41:28 +0600

    Update car's status when book a car using Transaction & Rollback

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,5 +1,5 @@
 import httpStatus from "http-status";
-import { Types } from "mongoose";
+import { startSession, Types } from "mongoose";
 import { JwtPayload } from "jsonwebtoken";
 import AppError from "../../errors/AppError";
 import { BookingProps } from "./booking.interface";
@@ -25,21 +25,50 @@ const createBookingService = async (
   if (!car) {
     throw new AppError(httpStatus.NOT_FOUND, "No Data Found");
   }
+  if (car.status !== "available") {
+    throw new AppError(httpStatus.NOT_FOUND, "Car is not available");
+  }
 
-  // Set userId and carId
-  payload.user = user._id;
-  payload.car = carId;
+  const session = await startSession();
+  try {
+    // start session
+    session.startTransaction();
 
-  // book a car
-  const newBooking = await BookingModel.create(payload);
+    // Set userId and carId
+    payload.user = user._id;
+    payload.car = carId;
 
-  // populate the booking with user & car details and send it to client
-  const result = await BookingModel.findById(newBooking._id)
-    .select("-__v")
-    .populate("user", "-password -createdAt -updatedAt -__v")
-    .populate("car", "-__v");
+    // book a car - (transaction 1)
+    const newBooking = await BookingModel.create([payload], { session });
+    if (!newBooking.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Fail to book a car");
+    }
 
-  return result;
+    // update car status(status = unavailable) - (transaction 2)
+    const updateCarStatus = await CarModel.findByIdAndUpdate(
+      carId,
+      { status: "unavailable" },
+      { session }
+    );
+    if (!updateCarStatus) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Fail to update car's status");
+    }
+
+    // populate the booking with user & car details and send it to client
+    const bookingId = newBooking[0]._id;
+    const result = BookingModel.findById(bookingId)
+      .select("-__v")
+      .populate("user", "-password -createdAt -updatedAt -__v")
+      .populate("car", "-__v");
+
+    await session.commitTransaction();
+    await session.endSession();
+    return result;
+  } catch (error: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(httpStatus.BAD_REQUEST, error.message);
+  }
 };
 
 // Get all bookings (Accessible to the Admin)
