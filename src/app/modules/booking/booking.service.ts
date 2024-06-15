d6a8844c9d4a7e60e72146d13806f27cb25f35d8
saiket-das <saiketdas02@gmail.com>
2024-06-15 07:00:32 +0600

    Add route to get all bookings (only accessible to Admin)

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,10 +1,10 @@
+import httpStatus from "http-status";
+import { Types } from "mongoose";
 import { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errors/AppError";
 import { BookingProps } from "./booking.interface";
 import { BookingModel } from "./booking.model";
 import { UserModel } from "../user/user.model";
-import AppError from "../../errors/AppError";
-import httpStatus from "http-status";
-import { Types } from "mongoose";
 import { CarModel } from "../car/car.model";
 
 // Book a car (Only accessible to the User)
@@ -30,12 +30,24 @@ const createBookingService = async (
   payload.user = user._id;
   payload.car = carId;
 
-  const result = (
-    await (await BookingModel.create(payload)).populate("user")
-  ).populate("car");
+  // book a car
+  const newBooking = await BookingModel.create(payload);
+
+  // populate the booking with user & car details and send it to client
+  const result = await BookingModel.findById(newBooking._id)
+    .populate("user", "-password")
+    .populate("car");
   return result;
 };
 
+const getAllBookingService = async () => {
+  const result = await BookingModel.find()
+    .populate("user", "-password")
+    .populate("car");
+
+  return result;
+};
 export const BookingServices = {
   createBookingService,
+  getAllBookingService,
 };
